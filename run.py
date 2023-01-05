01c25d2d3f33c3044f9ec3865d58afd380fcbd41
Cen Jinglun <cenjinglun@qq.com>
2023-01-06 01:42:22 +0800

    add db

--- run.py
+++ run.py
@@ -5,15 +5,12 @@ from flask_sqlalchemy import SQLAlchemy  # 导入扩展类
 from markupsafe import escape
 import click
 
-WIN = sys.platform.startswith("win")
-if WIN:  # 如果是 Windows 系统，使用三个斜线
-    prefix = "sqlite:///"
-else:  # 否则使用四个斜线
-    prefix = "sqlite:////"
+prefix = "sqlite:///" if sys.platform.startswith("win") else: "sqlite:////"
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(app.root_path, "data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # 关闭对模型修改的监控
+
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
 
