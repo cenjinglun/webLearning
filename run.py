4b3a65452f3cc726110e94be0d6a55c7d8b82046
Cen Jinglun <cenjinglun@qq.com>
2023-01-06 01:50:40 +0800

    add db

--- run.py
+++ run.py
@@ -10,9 +10,29 @@ prefix = "sqlite:///" if sys.platform.startswith("win") else "sqlite:////"
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = prefix + os.path.join(app.root_path, "data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # 关闭对模型修改的监控
-
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+@app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
+@click.option("--drop", is_flag=True, help="Create after drop.")  # 设置选项
+def initdb(drop):
+    """Initialize the database."""
+    if drop:  # 判断是否输入了选项
+        db.drop_all()
+    db.create_all()
+    click.echo("Initialized database.")  # 输出提示信息
 
 
 @app.cli.command()
@@ -43,17 +63,6 @@ def forge():
 
     db.session.commit()
     click.echo("Done.")
-
-
-class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    name = db.Column(db.String(20))  # 名字
-
-
-class Movie(db.Model):  # 表名将会是 movie
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    title = db.Column(db.String(60))  # 电影标题
-    year = db.Column(db.String(4))  # 电影年份
 
 
 @app.route("/")
