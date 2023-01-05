4870529d2b93861508105fb3e2d4981a21b53ff5
Cen Jinglun <cenjinglun@qq.com>
2023-01-06 01:45:41 +0800

    add db

--- run.py
+++ run.py
@@ -15,24 +15,6 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # 关闭对模型修改的
 db = SQLAlchemy(app)
 
 
-class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    name = db.Column(db.String(20))  # 名字
-
-
-class Movie(db.Model):  # 表名将会是 movie
-    id = db.Column(db.Integer, primary_key=True)  # 主键
-    title = db.Column(db.String(60))  # 电影标题
-    year = db.Column(db.String(4))  # 电影年份
-
-
-@app.route("/")
-def index():
-    user = User.query.first()  # 读取用户记录
-    movies = Movie.query.all()  # 读取所有电影记录
-    return render_template("index.html", user=user, movies=movies)
-
-
 @app.cli.command()
 def forge():
     """Generate fake data."""
@@ -61,6 +43,24 @@ def forge():
 
     db.session.commit()
     click.echo("Done.")
+
+
+class User(db.Model):  # 表名将会是 user（自动生成，小写处理）
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    name = db.Column(db.String(20))  # 名字
+
+
+class Movie(db.Model):  # 表名将会是 movie
+    id = db.Column(db.Integer, primary_key=True)  # 主键
+    title = db.Column(db.String(60))  # 电影标题
+    year = db.Column(db.String(4))  # 电影年份
+
+
+@app.route("/")
+def index():
+    user = User.query.first()  # 读取用户记录
+    movies = Movie.query.all()  # 读取所有电影记录
+    return render_template("index.html", user=user, movies=movies)
 
 
 # @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
